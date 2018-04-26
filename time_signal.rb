57d4ae87e88f8046b3cc7e6e33173c29d9a4fb95
森泉亮介 <izumiruryousuke@gmail.com>
2018-04-26 23:04:08 +0900

    feat:通知をもとに時間修正

--- time_signal.rb
+++ time_signal.rb
@@ -8,25 +8,25 @@ Slack.configure do |config|
 end
 
 case Time.now.hour
-when 9 then
+when 0 then
   message = '9時: おはようございます！今日の目標を明確にしましょう！'
-when 10 then
+when 1 then
   message = '10時: 午前は捗りますね。その調子です。今日１日101%成長していきましょう！'
-when 11 then
+when 2 then
   message = '11時: もうそろそろランチタイムです。あと少し頑張りましょう！'
-when 12 then
+when 3 then
   message = '12時: ランチタイムです。コンビニばかりにならないように！'
-when 13 then
+when 4 then
   message = '13時: 午後が始まりました。時間が早く流れるので気をつけて下さい！'
-when 14 then
+when 5 then
   message = '14時: 集中力が切れていませんか？そんな時はチョコとコーヒーです！'
-when 15 then
+when 6 then
   message = '15時: おやつなんて食べている場合ではありません。働いてください！'
-when 16 then
+when 7 then
   message = '16時: 午後も3時間経過、今日の目標は達成できていますか？'
-when 17 then
+when 8 then
   message = '17時: ここから切り替えて、さらにもうワンステップ先にいきましょう！'
-when 18 then
+when 9 then
   message = '18時: お疲れ様です。業務時間は終了です。今日の振り返りをしましょう。'
 end
 
@@ -35,4 +35,3 @@ Slack.chat_postMessage(text: message,
                        username: '時報bot',
                        icon_url: 'https://i.gyazo.com/763d65291a1bef993f7dac57ce7ec643.png'
                       )
-
