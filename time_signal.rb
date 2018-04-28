4349c2a34154d2d39fe071173215dacade9feddd
森泉亮介 <izumiruryousuke@gmail.com>
2018-04-28 19:58:11 +0900

    feat:TZ設定

--- time_signal.rb
+++ time_signal.rb
@@ -3,7 +3,7 @@
 require "rubygems"
 require "slack"
 
-SYSTEM_TZ="Asia/Japan"
+SYSTEM_TZ="Asia/Tokyo"
 
 Slack.configure do |config|
   config.token = 'xoxp-172951701027-173842403815-352138423553-ac5ca3fe8597e6319e0915b84c1b0389'
